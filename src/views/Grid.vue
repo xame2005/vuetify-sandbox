6a147a9071759b5cd1be9436bf6c7cca6f77c94f
Samuel Gonzalez <xame2005@gmail.com>
2021-03-30 22:27:38 -0600

    [CREATED] Grid component. Vuetify grid for responsive sites

---
+++ src/views/Grid.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12" sm="8">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero,
+          mollitia veritatis ducimus provident, autem necessitatibus facilis
+          repudiandae consequatur suscipit molestias omnis, dolor nam eius
+          incidunt! Doloremque sapiente beatae incidunt eaque.
+        </p>
+      </v-col>
+      <v-col cols="12" sm="4">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero,
+          mollitia veritatis ducimus provident, autem necessitatibus facilis
+          repudiandae consequatur suscipit molestias omnis, dolor nam eius
+          incidunt! Doloremque sapiente beatae incidunt eaque.
+        </p>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
